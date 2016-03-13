dcf1cfa86fc6e195a0aec9f3d7a6f34cf86c3459
Syntox32 <syntox32@gmail.com>
2016-03-13 02:33:13 +0100

    added history class

---
+++ Visualizer/History.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <list>
+
+class History
+{
+public:
+	History(size_t maxLen)
+		: maxLen(maxLen)
+	{ }
+
+	~History()
+	{
+		delete history;
+	}
+
+	void push(float val)
+	{
+		if (history->size() == maxLen)
+			history->pop_back();
+		
+		history->push_front(val);
+	}
+
+	float sum()
+	{
+		float ret = 0.0f;
+		
+		for (auto& val : *history)
+			ret += val;
+		
+		return ret;
+	}
+
+	float avg()
+	{
+		float sumVal = sum();
+		return (float)(sumVal / history->size());
+	}
+
+	inline std::list<float> *getHistory() const { return history; }
+	inline size_t getMaxLen() const { return maxLen; }
+
+private:
+	size_t maxLen;
+	std::list<float> *history;
+};
+
